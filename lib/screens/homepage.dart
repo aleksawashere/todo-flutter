654f959b411eb43c8d489829b5b33b2add1ae08c
aleksawashere <aleksa.dimitrijevic.business@gmail.com>
2022-08-04 13:28:36 +0200

    Added homepage with logo

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter/widgets.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(30),
+          color:Color(0xFFF6F6F6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, //everything starts from the left
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Image(
+                width: 60,
+                height: 60,
+                image: AssetImage('assets/images/logo.png'),
+              ),
+              TaskCardWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskCardWidget extends StatelessWidget {
+  const TaskCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Text("Hello world")
+    );
+  }
+}
